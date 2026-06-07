26aeacf60f226d547aa9f110995fa3671ee19eaa
Shubham-Poddar9 <poddarshubham258@gmail.com>
2026-06-07 16:02:54 +0545

    Add files via upload

--- face.py
+++ face.py
@@ -1,30 +1,32 @@
 import cv2
 
-face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+"haarcascade_frontalface_default.xml")
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 
 cap=cv2.VideoCapture(0)
-
 if not cap.isOpened():
-    print("error:could not open the camera")
+    print("error")
     exit()
 
 while True:
     ret,frame=cap.read()
 
     if not ret:
-        print("error couldn't capture the immage")
+        print("error")
         break
 
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     faces=face_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=5,minSize=(30,30))
 
     for(x,y,w,h) in faces:
         cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-    
-    cv2.imshow('face detation',frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    font=cv2.FONT_HERSHEY_SCRIPT_COMPLEX
+    cv2.putText(frame,f'People Count:{len(faces)} ',(10,70),font,3,(255,45,79),3,cv2.LINE_AA)
+
+    cv2.imshow("face tracking and counting",frame)
+
+    if cv2.waitKey(1) & 0xFF==ord('q'):
         break
 
 cap.release()
