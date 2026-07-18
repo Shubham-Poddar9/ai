0cabb092073abaeccca9f12184861ba3b8cc048e
Shubham-Poddar9 <poddarshubham258@gmail.com>
2026-07-18 10:09:19 +0545

    Add files via upload

--- sentiment.py
+++ sentiment.py
@@ -1,14 +1,34 @@
-from textblob import TextBlob
-import colorama
-from colorama import Fore
-colorama.init()
-text=input("please enter your text")
-a = TextBlob(text)
-b= a.sentiment.polarity
-if (b>0):
-    print(Fore.GREEN,"sentence is positive",b)
-elif (b<0):
-    print(Fore.RED,"sentence is negative",b)
-else:
-    print("neutral",b)
+import requests
 
+api ="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"
+
+url = "https://router.huggingface.co/hf-inference/models/sentence-transformers/all-MiniLM-L6-v2"
+
+headers ={ "Authorization": f"Bearer {api}"}
+
+while True:
+    a=input("enter your sentance 1")
+    if a.lower()== "exit":
+        break
+
+    b=input("enter your sentance 2")
+    if b.lower()=="exit":
+        break
+
+    payload ={ "inputs":{"source_sentence":a, "sentences":[b]}}
+
+    r= requests.post(url,headers=headers,json=payload)
+
+    if r.ok:
+        score=r.json()[0]
+        print("score similarity is ",score,"%")
+
+        if score >= 0.72:
+            print("its duplicate")
+
+        else:
+            print("its diffrent")
+
+
+    else:
+        print("error",r.text)
